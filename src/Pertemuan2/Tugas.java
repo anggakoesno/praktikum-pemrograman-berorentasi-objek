432a06efd3c38f85d91bf01710fdd95a359898f9
anggakoesno <rafaelanggajati@gmail.com>
2026-02-20 10:09:42 +0700

    tugas 1: tambah penjelasan

--- src/Pertemuan2/Tugas.java
+++ src/Pertemuan2/Tugas.java
@@ -6,50 +6,59 @@ public class Tugas {
 
     public static void main (String[] args) {
         // --- Nomor 1 ---
-//        Scanner input = new Scanner(System.in);
-//
-//        System.out.print("Masukkan size array: ");
-//        int size = input.nextInt();
-//
-//        int [] wadah = new int[size];
-//
-//        System.out.println("Masukkan angka array:");
-//
-//        for (int i = 0; i < size; i++) {
-//            System.out.print("Angka " + (i+1) + ": ");
-//            int angka = input.nextInt();
-//
-//            wadah[i] = angka;
-//        }
-//
-//        System.out.print("Final array: ");
-//        System.out.println(Arrays.toString(wadah));
-//
-//        int jumlah = 0;
-//        for (int nilai : wadah) {
-//            jumlah += nilai;
-//        }
-//
-//        double avg = (double) jumlah/wadah.length;
-//
-//        System.out.println("Rata-rata: " + avg);
-//
-//        String moreAVG = "";
-//
-//        for (int idx = 0; idx < wadah.length; idx++) {
-//            if (wadah[idx] > avg) {
-//                moreAVG += wadah[idx] + " ";
-//            }
-//        }
-//
-//        if (moreAVG.isEmpty()) {
-//            moreAVG = "Tidak ada angka yang lebih besar dari rata-rata.";
-//        }
-//
-//        System.out.print("Angka yang lebih besar dari rata-rata: " + moreAVG);
+        // Inisialisasi Scanner untuk menerima inputan
+        Scanner input = new Scanner(System.in);
+
+        // Menerima inputan size array
+        System.out.print("Masukkan size array: ");
+        int size = input.nextInt();
+
+        // Siapkan array wadah untuk menampung inputan daftar angka
+        int [] wadah = new int[size];
+
+        // Menerima inputan daftar angka
+        System.out.println("Masukkan angka array:");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Angka " + (i+1) + ": ");
+            int angka = input.nextInt(); // variabel angka akan menerima inputan angka
+
+            wadah[i] = angka; // memasukkan variabel angka ke dalam wadah sesuai index
+        }
+
+        // Menampilkan hasil array
+        System.out.print("Final array: ");
+        System.out.println(Arrays.toString(wadah));
+
+        // Menghitung jumlah seluruh angka dalam array untuk dipakai menghitung rata-rata
+        int jumlah = 0;
+        for (int nilai : wadah) {
+            jumlah += nilai;
+        }
+
+        // Menghitung rata-rata
+        double avg = (double) jumlah/wadah.length;
+        System.out.println("Rata-rata: " + avg);
+
+        // Siapkan string moreAVG untuk menampung angka yang melebihi rata-rata
+        String moreAVG = "";
+
+        // Memasukkan angka di dalam array yang lebih besar dari rata-rata
+        for (int idx = 0; idx < wadah.length; idx++) { // cek semua angka di dalam array
+            if (wadah[idx] > avg) { // jika angka tersebut > rata-rata
+                moreAVG += wadah[idx] + " "; // masukkan ke dalam variabel moreAVG
+            }
+        }
+
+        if (moreAVG.isEmpty()) { // jika variabel moreAVG kosong
+            moreAVG = "Tidak ada angka yang lebih besar dari rata-rata.";
+        }
+
+        // Menampilkan angka yang lebih besar dari rata-rata
+        System.out.print("Angka yang lebih besar dari rata-rata: " + moreAVG);
 
         // --- Nomor 2 ---
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
 
         System.out.print("Masukkan ukuran matriks: ");
         int ukuran = input.nextInt();
