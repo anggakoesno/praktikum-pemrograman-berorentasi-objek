e17a21f17e1e155f385f23b6d6876d85100dda47
anggakoesno <rafaelanggajati@gmail.com>
2026-02-21 07:57:40 +0700

    tugas 2: tambah penjelasan

--- src/Pertemuan2/Tugas.java
+++ src/Pertemuan2/Tugas.java
@@ -58,57 +58,64 @@ public class Tugas {
         System.out.print("Angka yang lebih besar dari rata-rata: " + moreAVG);
 
         // --- Nomor 2 ---
+        // Inisialisasi Scanner
 //        Scanner input = new Scanner(System.in);
 
+        // Menerima input ukuran matriks
         System.out.print("Masukkan ukuran matriks: ");
         int ukuran = input.nextInt();
 
+        // Menyiapkan array untuk menampung matriks
         int [][] matriks = new int[ukuran][ukuran];
 
-        for (int i = 0; i < ukuran; i++) {
-            boolean sah = false;
-            while (!sah) {
-                System.out.print("Masukkan angka untuk baris ke-" + (i + 1) + " (dipisahkan spasi): ");
-                String inputBaris = input.nextLine();
-                String[] angka = inputBaris.split(" ");
 
-                if (angka.length != ukuran) {
+        for (int i = 0; i < ukuran; i++) { // Perulangan untuk baris
+            boolean sah = false; // Anggapan apabila input pengguna salah
+            while (!sah) { // Selama input belum sah
+                System.out.print("Masukkan angka untuk baris ke-" + (i + 1) + " (dipisahkan spasi): ");
+                String inputBaris = input.nextLine(); // Menerima input daftar angka dalam bentuk string
+                String[] angka = inputBaris.split(" "); // Memotong string yang dipisahkan spasi
+
+                if (angka.length != ukuran) { // Jika banyak angka yang diinput tidak sesuai dengan ukuran yang ditentukan
                     System.out.println("Jumlah kolom tidak sesuai, silakan masukkan " + ukuran + " buah angka!");
-                } else {
-                    for (int j = 0; j < ukuran; j++) {
-                        matriks[i][j] = Integer.parseInt(angka[j]);
+                } else { // Jika banyak angka yang diinput sesuai dengan ukuran yang ditentukan
+                    for (int j = 0; j < ukuran; j++) { // Perulangan untuk kolom
+                        // Masukkan angka-angka untuk 1 baris (dengan beberapa kolom)
+                        matriks[i][j] = Integer.parseInt(angka[j]); // parseInt = mengubah string angka menjadi integer
                     }
                     sah = true;
                 }
             }
         }
 
-        int nilaiMaks = matriks[0][0];
+        // Deklarasi dan inisialisasi variabel untuk menampung nilai terbesar, nilai terkecil, dan jumlah diagonal utama
+        int nilaiMaks = matriks[0][0]; // nilai matriks di baris sekian dan kolom sekian
         int nilaiMin = matriks[0][0];
         int diagonalUtama = 0;
 
         System.out.println("\nHasil matriks: ");
-        for (int b = 0; b < ukuran; b++) {
-            for (int k = 0; k < ukuran; k++) {
+        for (int b = 0; b < ukuran; b++) { // Perulangan baris
+            for (int k = 0; k < ukuran; k++) { // Perulangan kolom
                 int nilai = matriks[b][k];
                 System.out.print(nilai + " ");
 
 
-                if (nilai > nilaiMaks) {
-                    nilaiMaks = nilai;
+                if (nilai > nilaiMaks) { // Jika nilai di matriks[b][k] > matriks[0][0]
+                    nilaiMaks = nilai; // Perbarui nilaiMaks => matriks[0][0] = matriks[b][k]
                 }
 
-                if (nilai < nilaiMin) {
-                    nilaiMin = nilai;
+                if (nilai < nilaiMin) { // Jika nilai di matriks[b][k] < matriks[0][0]
+                    nilaiMin = nilai; // Perbarui nilaiMin => matriks[0][0] = matriks[b][k]
                 }
 
-                if (b == k) {
-                    diagonalUtama += nilai;
+                if (b == k) { // Jika nilai matriks berada di posisi NN == diagonal utama
+                    diagonalUtama += nilai; // Perbarui jumlah diagonal utama => diagonalUtama = diagonalUtama + matriks[b][k]
                 }
             }
-            System.out.println();
+            System.out.println(); // Membuat baris baru setelah perulangan kolom terselesaikan
         }
 
+        // Menampilkan hasil
         System.out.println("Nilai maksimal adalah : " + nilaiMaks);
         System.out.println("Nilai minimum adalah : " + nilaiMin);
         System.out.println("Jumlah diagonal utama adalah : " + diagonalUtama);
