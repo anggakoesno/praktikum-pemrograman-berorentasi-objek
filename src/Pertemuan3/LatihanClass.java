ac82e6f2675e90aebe0f800ef371283c317fe3c3
anggakoesno <rafaelanggajati@gmail.com>
2026-02-26 08:58:25 +0700

    buat objek Game

--- src/Pertemuan3/LatihanClass.java
+++ src/Pertemuan3/LatihanClass.java
@@ -2,7 +2,12 @@ package Pertemuan3;
 
 public class LatihanClass {
     public static void main(String[] args) {
-        LatihanClass.printedData(0, "Data1", "Data2", "Data3");
+        // LatihanClass.printedData(0, "Data1", "Data2", "Data3");
+
+        Game darts = new Game();
+        Game pinball = new Game(20);
+        System.out.println("Score darts: " + darts.score);
+        System.out.println("Score pinball: " + pinball.score);
     }
 
     public static void printedData(int number, String... varg) {
