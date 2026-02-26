d0d96299f348a713567ae30e9f506c41ea844dd7
anggakoesno <rafaelanggajati@gmail.com>
2026-02-26 08:57:59 +0700

    inisialisasi Game

---
+++ src/Pertemuan3/Game.java
@@ -0,0 +1,14 @@
+package Pertemuan3;
+
+public class Game {
+    int score;
+
+    // Constructor
+    Game() {
+        score = 0;
+    }
+
+    Game(int startingScore) {
+        score = startingScore;
+    }
+}
