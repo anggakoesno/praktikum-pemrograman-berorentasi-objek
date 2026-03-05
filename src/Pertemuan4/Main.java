6ce13100d849547526e78c4b11a140842a2a4f21
anggakoesno <rafaelanggajati@gmail.com>
2026-03-05 08:41:56 +0700

    inisialisasi file Main dan Person pertemuan 4 modifier

---
+++ src/Pertemuan4/Main.java
@@ -0,0 +1,9 @@
+package Pertemuan4;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.address = "Jogja";
+        person.sayAddress();
+    }
+}
---
+++ src/Pertemuan4/Person.java
@@ -0,0 +1,9 @@
+package Pertemuan4;
+
+public class Person {
+    public String address;
+    public void sayAddress() {
+        System.out.println("My address is " + address);
+    }
+
+}
