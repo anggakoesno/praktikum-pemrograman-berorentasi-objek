d9bb61cf60ece64517c0b87371927efb2cb36c65
anggakoesno <rafaelanggajati@gmail.com>
2026-03-05 08:52:12 +0700

    percobaan perubahan modifier class Person

--- src/Pertemuan4/Main.java
+++ src/Pertemuan4/Main.java
@@ -3,7 +3,7 @@ package Pertemuan4;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
-        person.address = "Jogja";
+        person.setAddress("Jogja");
         person.sayAddress();
     }
 }
--- src/Pertemuan4/Person.java
+++ src/Pertemuan4/Person.java
@@ -1,7 +1,12 @@
 package Pertemuan4;
 
 public class Person {
-    public String address;
+    private String address;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void sayAddress() {
         System.out.println("My address is " + address);
     }
