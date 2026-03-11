e7a146e05201d97363a42511926c42f4c9d577d3
anggakoesno <rafaelanggajati@gmail.com>
2026-03-11 17:12:41 +0700

    subclass Flazzy

--- src/Pertemuan5_PBO/Flazzy.java
+++ src/Pertemuan5_PBO/Flazzy.java
@@ -1,4 +1,17 @@
 package Pertemuan5_PBO;
 
-public class Flazzy {
+public class Flazzy extends Emoney{
+    private double extraTopUp = 0.1;
+
+    public Flazzy(String id, double balance, String edition) {
+        super(id, balance, edition);
+    }
+
+    @Override
+    public double topUp(int amount) {
+        double extra = amount * extraTopUp;
+        balance += amount + extra;
+        System.out.println("Berhasil top up sebesar: Rp" + amount + " dan extra 10%: Rp" + extra);
+        return balance;
+    }
 }
