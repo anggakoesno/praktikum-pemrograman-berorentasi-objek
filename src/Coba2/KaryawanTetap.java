03a2c41bfdc117975ee51f6e906be454e3588905
anggakoesno <rafaelanggajati@gmail.com>
2026-03-19 09:30:45 +0700

    subclass KaryawanTetap

--- src/Coba2/KaryawanTetap.java
+++ src/Coba2/KaryawanTetap.java
@@ -1,4 +1,23 @@
 package Coba2;
 
-public class KaryawanTetap {
+public class KaryawanTetap extends Karyawan {
+    private double tunjangan;
+
+    public KaryawanTetap (String nama, double gajiPokok, double tunjangan) {
+        super(nama, gajiPokok);
+        this.tunjangan = tunjangan;
+    }
+
+    public double getTunjangan() {
+        return tunjangan;
+    }
+
+    @Override
+    public String tampilkanData() {
+        return "Nama: " + getNama() +
+                "\nGaji Pokok: " + getGajiPokok() +
+                "\nTunjangan: " + getTunjangan() +
+                "\nTotal Gaji: " + (getGajiPokok() + getTunjangan()) +
+                "\nStatus: " + StatusKaryawan.aktif;
+    }
 }
