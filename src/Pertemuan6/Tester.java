bc5b74cd933b1256791b09e9c9b6ec0b8823b914
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 08:10:22 +0700

    polymorphism: tester (subclass)

--- src/Pertemuan6/Tester.java
+++ src/Pertemuan6/Tester.java
@@ -1,4 +1,12 @@
 package Pertemuan6;
 
-public class Tester {
+public class Tester extends Employee {
+    public Tester (String name) {
+        super(name);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(name + " is testing the application.");
+    }
 }
