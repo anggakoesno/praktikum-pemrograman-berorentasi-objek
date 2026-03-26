d6810c82159aced57d7951d27208fc5fa8a2309a
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 08:09:48 +0700

    polymorphism: programmer (subclass)

--- src/Pertemuan6/Programmer.java
+++ src/Pertemuan6/Programmer.java
@@ -1,4 +1,12 @@
 package Pertemuan6;
 
-public class Programmer {
+public class Programmer extends Employee {
+    public Programmer (String name) {
+        super(name);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(name + " is writing Java Code.");
+    }
 }
