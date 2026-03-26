9363b197defe5fa3af95847a3000a0962395dbde
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 08:35:07 +0700

    add: class employee in demo

--- src/Pertemuan6/PolymorphismDemo.java
+++ src/Pertemuan6/PolymorphismDemo.java
@@ -3,6 +3,7 @@ package Pertemuan6;
 public class PolymorphismDemo {
     public static void main(String[] args) {
         Employee[] team = {
+                new Employee("Dono"),
                 new Programmer("Andi"),
                 new Designer("Budi"),
                 new Tester("Citra")
