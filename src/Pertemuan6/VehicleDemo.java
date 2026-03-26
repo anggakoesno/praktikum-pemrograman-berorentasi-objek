fd064d39974de592d4b82e02b175c73b86604325
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 09:11:17 +0700

    abstract & interface: vehicle demo (main class)

--- src/Pertemuan6/VehicleDemo.java
+++ src/Pertemuan6/VehicleDemo.java
@@ -1,4 +1,27 @@
 package Pertemuan6;
 
 public class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+                new Car("Toyota", 180),
+                new ElectricCar("Tesla Model 3", 225),
+                new Drone("DJI Phantom", 72)
+        };
+
+        for (Vehicle v : vehicles) {
+            v.showBasicInfo();
+            v.startEngine();
+            v.move();
+
+            if (v instanceof Electric) {
+                ((Electric) v).chargeBattery();
+            }
+
+            if (v instanceof Flyable) {
+                ((Flyable) v).fly();
+            }
+
+            System.out.println();
+        }
+    }
 }
