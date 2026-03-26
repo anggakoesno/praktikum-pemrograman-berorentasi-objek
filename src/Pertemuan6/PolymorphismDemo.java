0fff6dd278f7a249088a5ba165ebf8d8dedaaa7a
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 08:11:07 +0700

    polymorphism: polymorpishm demo (main class)

--- src/Pertemuan6/PolymorphismDemo.java
+++ src/Pertemuan6/PolymorphismDemo.java
@@ -1,4 +1,15 @@
 package Pertemuan6;
 
 public class PolymorphismDemo {
+    public static void main(String[] args) {
+        Employee[] team = {
+                new Programmer("Andi"),
+                new Designer("Budi"),
+                new Tester("Citra")
+        };
+
+        for (Employee emp : team) {
+            emp.work();
+        }
+    }
 }
