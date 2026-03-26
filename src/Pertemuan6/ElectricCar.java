e0e24e24bc9be9232c05821e38b2bc9d2cf6cdbd
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 09:09:56 +0700

    abstract & interface: electric car (vehicle & implement electric)

--- src/Pertemuan6/ElectricCar.java
+++ src/Pertemuan6/ElectricCar.java
@@ -1,4 +1,17 @@
 package Pertemuan6;
 
-public class ElectricCar {
+class ElectricCar extends Vehicle implements Electric {
+    public ElectricCar(String name, int maxSpeed) {
+        super(name, maxSpeed);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + " moves silently using electric power.");
+    }
+
+    @Override
+    public void chargeBattery() {
+        System.out.println(name + " battery is charging.");
+    }
 }
