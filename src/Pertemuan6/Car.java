37d014773ca2de45b7f1333b993147f124993489
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 09:08:52 +0700

    abstract & interface: car (umum)

--- src/Pertemuan6/Car.java
+++ src/Pertemuan6/Car.java
@@ -1,4 +1,12 @@
 package Pertemuan6;
 
-public class Car {
+class Car extends Vehicle{
+    public Car(String name, int maxSpeed) {
+        super(name, maxSpeed);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + " moves on the road.");
+    }
 }
