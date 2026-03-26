a41fa4b364cd2a2fcd6bd3352bcc5cddb4c8c679
anggakoesno <rafaelanggajati@gmail.com>
2026-03-26 09:10:43 +0700

    abstract & interface: drone (vehicle & implement electric, flyable)

--- src/Pertemuan6/Drone.java
+++ src/Pertemuan6/Drone.java
@@ -1,4 +1,22 @@
 package Pertemuan6;
 
-public class Drone {
+class Drone extends Vehicle implements Electric, Flyable {
+    public Drone(String name, int maxSpeed) {
+        super(name, maxSpeed);
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + " moves by rotating propellers.");
+    }
+
+    @Override
+    public void chargeBattery() {
+        System.out.println(name + " battery is charging.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(name + " is flying in the air.");
+    }
 }
